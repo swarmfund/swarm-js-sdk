b0c92de643616630c6079ab6e497518d91935e9f
Volodymyr Bochko <v.y.bochko@gmail.com>
2017-12-22 19:07:27 +0200

    Move master secret keys to master_seeds.

--- scripts/system-init/make_general.js
+++ scripts/system-init/make_general.js
@@ -1,7 +1,9 @@
 const helpers = require('./../helpers');
-const config = require('./config')
+const config = require('../config')
 const StellarSdk = require('../../lib/index');
 
+let env = 'dev';
+let currentConfig = config.getConfig(env);
 
-helpers.accounts.createNewAccount(config, "GDA2SZAWRC64BILEYP4G6KRRHM64FKMRNYKSHDLNQUQ7ZS6DK5377QF2", StellarSdk.xdr.AccountType.general().value, 0)
+helpers.accounts.createNewAccount(currentConfig, "GDA2SZAWRC64BILEYP4G6KRRHM64FKMRNYKSHDLNQUQ7ZS6DK5377QF2", StellarSdk.xdr.AccountType.general().value, 0)
 .then(()=> {console.log("success")}).catch(helpers.errorHandler);
